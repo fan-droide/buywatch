731021c5cb1e373e34aa85b084ab0f283e232261
josemanuel <g.josemanuel@ext.douglas.eu>
2023-04-01 01:09:54 +0200

    fix git ignore index.html

--- src/js/main.js
+++ src/js/main.js
@@ -4,12 +4,7 @@
 // Import only the Bootstrap components we need
 import { Carousel } from 'bootstrap';
 
-// Create an example popover
-// document.querySelectorAll('[data-bs-toggle="popover"]')
-//   .forEach(popover => {
-//     new Popover(popover)
-//   })
-//const carousel = new bootstrap.Carousel('#myCarousel')
+
 const isCarousel = document.getElementById('myCarousel')
 if(isCarousel){
     const carousel = new Carousel('#myCarousel')
